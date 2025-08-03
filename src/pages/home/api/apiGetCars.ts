95799d57d4f21b2d2e609a198fd9020ddb257b71
Oleg Kuzmin <info@oleg-kuzmin.ru>
2025-08-03 13:11:28 +0300

    fix: change api endpoint

--- src/pages/home/api/apiGetCars.ts
+++ src/pages/home/api/apiGetCars.ts
@@ -2,7 +2,7 @@ import type { Car } from '../model';
 
 export async function apiGetCars(): Promise<Car[]> {
   try {
-    const res = await fetch('https://ofc-test-01.tspb.su/test-task/vehicles');
+    const res = await fetch('https://6732ff9a2a1b1a4ae111a3bd.mockapi.io/api/car-tracker');
     const data = await res.json();
     return data;
   } catch (error) {
